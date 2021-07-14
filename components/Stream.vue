d069ff7efc56ed3f88cfa28af7bba5b9893d933c
Isaacddr <isaacovc00@gmail.com>
2021-07-14 12:15:17 -0500

    Radio mast

--- components/Stream.vue
+++ components/Stream.vue
@@ -3,6 +3,10 @@
     <!--
       <iframe title="stream" class=" lg:mx-auto w-full lg:w-1/2" src="https://streams.radiomast.io/station/293d34d7-41a4-469f-b1b4-3abdb29e9498/player/widget?t=dark&s=668aa56b-5094-4326-a395-db9dfad740e1" width="auto" height="380" scrolling="no" frameborder="0" allow="autoplay"></iframe>
     -->
-    <audio controls class="mx-auto w-3/4" src="#"></audio>
+    <audio
+      controls
+      class="mx-auto w-3/4"
+      src="https://streams.radiomast.io/4af4e331-a5d1-4cfc-a5c7-dfd5d78e38e7"
+    ></audio>
   </div>
 </template>
